879443d881e5a8f4c829480c677d9e476b53c1cb
chwahajahmad <chwahajahmad@gmail.com>
2021-08-09 20:05:06 +0500

    DB SETUP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+const express = require('express');
+const postgre = require('./db.config');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => console.log('Running Successfully at 3000'));
