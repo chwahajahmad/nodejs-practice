c18d641a6e4e2eae2f7b2f96b269f8b7c5a34f26
Ahmad Wahaj <chwahajahmad@gmail.com>
2021-08-12 13:15:10 +0500

    Cron Job Recurrence Time Issue Fixed

--- src/ScheduledJobs/scheduler.js
+++ src/ScheduledJobs/scheduler.js
@@ -1,6 +1,6 @@
 const schedule = require('node-schedule');
 const { getSaveData, deleteAllData } = require('./Jobs/save-delete-data');
-module.exports = schedule.scheduleJob('*/1 * * * *', () => {
+module.exports = schedule.scheduleJob('0 23 * * */7', () => {
   deleteAllData();
   getSaveData();
 });
