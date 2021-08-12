2fd1f21522b05c73fd45d4da50b194e6c6140dd6
Ahmad Wahaj <chwahajahmad@gmail.com>
2021-08-12 13:10:57 +0500

    DAO Error removing

--- src/ScheduledJobs/Jobs/save-delete-data.js
+++ src/ScheduledJobs/Jobs/save-delete-data.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 const prayerTimeController = require('../../controller/prayerTime.controller');
-const { deleteAll } = require('../../DAOs/prayerTime.dao');
 const apiEndPoint = 'https://api.pray.zone/v2/times/this_week.json?city=';
 
 const getSaveData = (city = 'lahore') => {
--- src/ScheduledJobs/scheduler.js
+++ src/ScheduledJobs/scheduler.js
@@ -1,6 +1,6 @@
 const schedule = require('node-schedule');
 const { getSaveData, deleteAllData } = require('./Jobs/save-delete-data');
-module.exports = schedule.scheduleJob('* * * * * 7', () => {
+module.exports = schedule.scheduleJob('*/1 * * * *', () => {
   deleteAllData();
   getSaveData();
 });
