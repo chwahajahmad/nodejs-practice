7a32e94747875fefd3213d1a6638972e22f507d2
Ahmad Wahaj <chwahajahmad@gmail.com>
2021-08-23 11:17:18 +0500

    / route added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const postgre = require('./db.conn');
-// const scheduler = require('./ScheduledJobs/scheduler');
+const scheduler = require('./ScheduledJobs/scheduler');
+
 const app = express();
 const port = process.env.PORT || 3002;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello1'));
 app.listen(port, () => console.log(`Running Successfully at ${port}`));
