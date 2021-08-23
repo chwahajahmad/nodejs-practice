823e0f69513849d4c292ffb7062324c1267b1905
Ahmad Wahaj <chwahajahmad@gmail.com>
2021-08-23 19:51:35 +0500

    Cron Job For Daily Reminder

--- src/ScheduledJobs/scheduler.js
+++ src/ScheduledJobs/scheduler.js
@@ -1,6 +1,17 @@
 const schedule = require('node-schedule');
 const { getSaveData, deleteAllData } = require('./Jobs/save-delete-data');
-module.exports = schedule.scheduleJob('0 23 * * */7', () => {
-  deleteAllData();
-  getSaveData();
-});
+
+module.exports.weeklyDataGetterScheduler = schedule.scheduleJob(
+  '50 23 * * */6',
+  () => {
+    deleteAllData();
+    getSaveData();
+  },
+);
+
+module.exports.dailyReminderScheduler = schedule.scheduleJob(
+  '0 12 1-31 */1 *',
+  () => {
+    console.log('Hello');
+  },
+);
