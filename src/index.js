1d540acf3e595c14f83905e7555fec6b34222c8f
Ahmad Wahaj <chwahajahmad@gmail.com>
2021-08-30 11:33:26 +0500

    Home Route Response changed

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ const port = process.env.PORT || 3002;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello1'));
+app.get('/', (req, res) => res.send('Welcome TO Slack Prayer Bot!'));
 app.listen(port, () => console.log(`Running Successfully at ${port}`));
