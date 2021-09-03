d538d5e9de91e36b8b3c603379ddd4fcb992b130
Ahmad Wahaj <chwahajahmad@gmail.com>
2021-09-03 17:28:39 +0500

    Crontab error fixed

--- src/ScheduledJobs/scheduler.js
+++ src/ScheduledJobs/scheduler.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule');
 const { getSaveData, deleteAllData } = require('./Jobs/save-delete-data');
 const { setReminderForAll } = require('./Jobs/dailyReminders');
 module.exports.weeklyDataGetterScheduler = schedule.scheduleJob(
-  '50 23 * * */6',
+  '02 12 * * */7',
   () => {
     deleteAllData();
     getSaveData();
