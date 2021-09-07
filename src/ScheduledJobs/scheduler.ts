47c98f505eeb3a3086caf61e1423207ca2323d7f
Ahmad Wahaj <chwahajahmad@gmail.com>
2021-09-08 00:07:43 +0500

    scheduler error removed

--- src/ScheduledJobs/scheduler.ts
+++ src/ScheduledJobs/scheduler.ts
@@ -1,7 +1,7 @@
-export {};
-const schedule = require('node-schedule');
-const { getSaveData, deleteAllData } = require('./Jobs/save-delete-data');
-const { setReminderForAll } = require('./Jobs/dailyReminders');
+
+import schedule from 'node-schedule';
+import { getSaveData, deleteAllData } from './Jobs/save-delete-data';
+import { setReminderForAll } from './Jobs/dailyReminders';
 
 const weeklyDataGetterScheduler = schedule.scheduleJob('02 00 * * */7', () => {
   deleteAllData();
@@ -10,7 +10,7 @@ const weeklyDataGetterScheduler = schedule.scheduleJob('02 00 * * */7', () => {
   timezone:'Asia/Karachi'
 });
 
-const dailyReminderScheduler = schedule.scheduleJob('05 00 1-31 */1 *', () => {
+const dailyReminderScheduler = schedule.scheduleJob('11 00 1-31 */1 *', () => {
   setReminderForAll();
 },{
   timezone:'Asia/Karachi'
