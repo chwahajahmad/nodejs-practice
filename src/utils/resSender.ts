ce79d0693e7a129290752bfe5ed9cce5212b8022
Ahmad Wahaj <chwahajahmad@gmail.com>
2021-09-07 20:55:56 +0500

    Sending second interactive response due to slack api restriction

--- src/utils/resSender.ts
+++ src/utils/resSender.ts
@@ -1,7 +1,7 @@
-import { Response } from 'express';
-const sendRes = (res: Response, message: string, status: number = 200) => {
-  if (!res || !status || !message) return;
-  res.status(status).json({
+import axios from 'axios';
+const sendRes = (responseUrl: string, message: string) => {
+  if (!message) return;
+  axios.post(responseUrl, {
     text: message,
   });
 };
