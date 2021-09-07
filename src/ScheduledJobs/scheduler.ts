6313adff44a044ac4717e08c9619d63c35ac62e8
Ahmad Wahaj <chwahajahmad@gmail.com>
2021-09-08 01:32:53 +0500

    cron job fix

--- src/ScheduledJobs/scheduler.ts
+++ src/ScheduledJobs/scheduler.ts
@@ -1,19 +1,36 @@
 
-const schedule = require ('node-schedule');
+const CronJob = require('cron').CronJob;
 import { getSaveData, deleteAllData } from './Jobs/save-delete-data';
 import { setReminderForAll } from './Jobs/dailyReminders';
-
-const weeklyDataGetterScheduler = schedule.scheduleJob('40 00 * * */7', () => {
-  deleteAllData();
-  getSaveData();
-}, {
-  timezone:'Asia/Karachi'
+const weeklyDataGetterScheduler = new CronJob({
+  // Run at 05:00 Central time, only on weekdays
+  cronTime: '40 00 * * */7',
+  onTick: function() {
+      console.log('CronJob ran!!');
+  },
+  start: true,
+  timeZone: 'Asia/Karachi'
 });
-
-const dailyReminderScheduler = schedule.scheduleJob('26 01 1-31 */1 *', () => {
-  setReminderForAll();
-},{
-  timezone:'Asia/Karachi'
+const dailyReminderScheduler = new CronJob({
+  // Run at 05:00 Central time, only on weekdays
+  cronTime: '35 01 1-31 */1 *',
+  onTick: function() {
+      console.log('CronJob ran!!');
+  },
+  start: true,
+  timeZone: 'Asia/Karachi'
 });
+// const weeklyDataGetterScheduler = schedule.scheduleJob('', () => {
+//   deleteAllData();
+//   getSaveData();
+// }, {
+//   timezone:'Asia/Karachi'
+// });
+
+// const dailyReminderScheduler = schedule.scheduleJob('26 01 1-31 */1 *', () => {
+//   setReminderForAll();
+// },{
+//   timezone:'Asia/Karachi'
+// });
 
 export { weeklyDataGetterScheduler, dailyReminderScheduler };
