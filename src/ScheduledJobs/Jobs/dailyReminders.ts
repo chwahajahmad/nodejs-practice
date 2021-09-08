17258c00c81384d14227568ea6b2821c110cb291
Ahmad Wahaj <chwahajahmad@gmail.com>
2021-09-08 11:22:48 +0500

    reminders time set

--- src/ScheduledJobs/Jobs/dailyReminders.ts
+++ src/ScheduledJobs/Jobs/dailyReminders.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone' // dependent on utc plugin
+import {getScheduledMessages} from '../../services/slackTasks'
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
@@ -22,15 +23,15 @@ const setReminder = async (city: string, fiqah: string, channel: string) => {
 
   prayerTimes.forEach((namazName) => {
     const timezone = res[0].dataValues.data.location.timezone;
-    const timeStampNow = dayjs().unix();
-    const day = dayjs().day();
+    const timeNow = dayjs().format('YYYY-MM-DD HH:mm').toString();
+    const timeStampNow = dayjs.tz(timeNow, timezone).unix();
+    const day = dayjs.tz(timeNow, timezone).day();
     const time = res[0].dataValues.data.datetime[day].times[namazName];
     const date = res[0].dataValues.data.datetime[day].date.gregorian;
     const message = `Its ${namazName} Time`;
     
+    
     const timeStamp = dayjs.tz(`${date} ${time}`,timezone).unix();
-    console.log("Server data:",dayjs().unix());
-    console.log("came here", timeStamp,timeStampNow)
     if (timeStampNow < timeStamp) {
       try {
         postMessage(message, channel, timeStamp);
@@ -43,7 +44,7 @@ const setReminder = async (city: string, fiqah: string, channel: string) => {
 const setReminderForAll = async () => {
   const [err, userData] = await to(users.findAll());
   if (err) throw Error(err);
-  console.log("came in set reminder");
+
   userData.forEach((data: any) => {
     const { city, fiqah, slack_id } = data.dataValues;
     setReminder(city, fiqah, slack_id);
