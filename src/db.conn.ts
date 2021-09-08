5b7a618bf297353d6aaa08d5c7a11a889a35bfb2
Ahmad Wahaj <chwahajahmad@gmail.com>
2021-09-08 13:24:10 +0500

    database ssl turned on

--- src/db.conn.ts
+++ src/db.conn.ts
@@ -2,14 +2,23 @@ const Sequelize = require('sequelize');
 import * as config from './config/config.json';
 
 let databaseUrl,
-  creds = config['development'];
-  process.env.NODE_ENV === 'production'
-  ? (databaseUrl = `${process.env.DATABASE_URL}?sslmode=require`)
-  : (databaseUrl = `postgres://${creds.username}:${creds.password}@${creds.host}:${creds.port}/${creds.database}`);
+creds = config['development'];
+process.env.NODE_ENV === 'production'
+? (databaseUrl = `${process.env.DATABASE_URL}?sslmode=require`)
+: (databaseUrl = `postgres://${creds.username}:${creds.password}@${creds.host}:${creds.port}/${creds.database}`);
 
-  
+   
 
-const postgresConn = new Sequelize(databaseUrl);
+const postgresConn = new Sequelize(databaseUrl,{
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // very important
+    }
+  }
+});
 postgresConn
   .authenticate()
   .then(() => {
